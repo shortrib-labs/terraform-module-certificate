ff96d92621b9d9a5111cc35b5f432b9c1423964d
Chuck D'Antonio <cdantonio@pivotal.io>
2021-04-01 11:58:15 -0400

    Updates dependencies for ACME module
    TL;DR
    -----
    
    Finds the ACME provider where Hashicorp relocated it
    
    Details
    -------
    
    Since Terraform now supports a heirarchical namespace of providers,
    they appear to have moved the "acme" provider ourside of their
    namespace and back to the `vancluever` namespace. That change
    broke this module.
    
    This change addresses that change by refererencing the provider in
    it's new namespaces as a required provider for module.

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    acme = {
+      source  = "vancluever/acme"
+    }
+  }
+}
+
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
